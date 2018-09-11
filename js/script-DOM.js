9f6641214c81a6a76ec7ba314e8f8cf07da53ee3
Kiet <bunbothomngon@gmail.com>
2018-09-11 22:11:02 +0300

    added: mouseover to stop, fixed: all fn

--- js/script-DOM.js
+++ js/script-DOM.js
@@ -1,52 +1,90 @@
-let intervalID;
+let intervalID = {};
+const elements = {
+  inputField: document.getElementById('js-input'),
+  itemsContainer: document.querySelector('.generated-items'),
+  generateButton: document.querySelector('.js-generate'),
+  stopButton: document.querySelector('.js-stop'),
+  copyButton: document.querySelector('.js-copy'),
+  copyCodeValue: document.querySelector('.js-hex-code')
+};
 
 // Generate random hexadecimal
 function randomHexaNumberGenerator() {
-  let hexColor = '#';
-
-  let hexCode = Array(6)
-    .fill()
-    .map(code => {
-      let hexRandomNumber = Math.round(Math.random() * 16).toString(32);
-      return (code = hexRandomNumber);
-    });
-
-  return (hexColor += hexCode.join(''));
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
 const markup = `
   <div class="js-bg-color generated-item">
-    <p class="js-hex-code"></p>
-    <button class="btn btn-copy js-copy">Copy</button>
+    <p class="js-hex-code text-color"></p>
+    <button class="btn btn-copy js-copy hide">Copy</button>
   </div>
 `;
 
 function addElement() {
-  const items = document.querySelector('.generated-items');
-  const inputValue = document.getElementById('js-input').value;
+  let inputValue = elements.inputField.value;
+  const items = elements.itemsContainer;
+  items.innerHTML = ' ';
+
+  // By default, generator generates 5 divs with random background colors, when the input field has a value lower than 5.
+  inputValue <= 5 ? (inputValue = 5) : (inputValue = elements.inputField.value);
 
   for (let index = 0; index < inputValue; index++) {
     items.insertAdjacentHTML('afterbegin', markup);
-    changeBgColor();
   }
 }
 
-function setBgColor() {
-  const bgColorList = document.querySelectorAll('.js-bg-color');
-
-  bgColorList.forEach(bgColor => {
-    bgColor.children[0].textContent = bgColor.style.backgroundColor = randomHexaNumberGenerator();
-  });
+function setBgColor(item) {
+  item.children[0].textContent = item.style.backgroundColor = randomHexaNumberGenerator();
+  if (item.children[1].classList.contains('hide')) {
+    item.children[1].classList.remove('hide');
+  }
 }
 
 function changeBgColor() {
-  intervalID = setInterval(setBgColor, 2000);
+  const items = elements.itemsContainer;
+  let itemsArr = Array.from(items.children);
+
+  itemsArr.forEach((item, itemIndex) => {
+    // Adds id for each div
+    item.setAttribute('id', itemIndex + 1);
+
+    // Clears interval before set new
+    if (intervalID[itemIndex]) {
+      clearInterval(intervalID[itemIndex]);
+    }
+    intervalID[itemIndex] = setInterval(() => setBgColor(item), 3000);
+
+    // Stops generate random bg color
+    item.addEventListener('mouseover', () => {
+      clearInterval(intervalID[itemIndex]);
+    });
+  });
 }
 
 function stopInterval() {
   clearInterval(intervalID);
 }
 
-document.querySelector('.js-generate').addEventListener('click', addElement);
+function clearInput() {
+  elements.inputField.value = '';
+}
 
-document.querySelector('.js-stop').addEventListener('click', stopInterval);
+/** On initialize **/
+function init() {
+  if (elements.inputField.value && elements.inputField.value > 0) {
+    addElement();
+    changeBgColor();
+    clearInput();
+  } else {
+    alert('Please provide a valid number.');
+  }
+}
+
+// Generates numbers of div with random hexcolor code
+elements.generateButton.addEventListener('click', init);
+
+// Stops generate random background color for all divs
+elements.stopButton.addEventListener('click', stopInterval);
+
+// Copy hexcode
+// elements.copyButton.addEventListener('click', clickToCopy);
